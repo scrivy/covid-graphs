5619634fcf0848e9fb043777154a296111e53e57
scrivy <daniel@danielscrivano.com>
2020-04-08 02:10:01 -0700

    added cronjob.sh

---
+++ cronjob.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd covid-graphs
+
+git log -1 > last-commit
+git submodule status > last-submodule
+
+git pull -q
+git submodule update --remote
+
+git log -1 > current-commit
+git submodule status > current-submodule
+
+if ! diff -q {last,current}-commit || ! diff -q {last,current}-submodule
+then
+	date
+	echo 'generating graphs.....'
+	~/.local/bin/pipenv run python generate_graphs.py
+	echo 'done'
+fi
